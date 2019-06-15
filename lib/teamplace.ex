461be943e1b89d3997acd19fe78543516f9637ca
Jose Esteves <jose@ceibo.co>
2019-06-15 11:51:59 -0300

    :art: 0.2.1

--- lib/teamplace.ex
+++ lib/teamplace.ex
@@ -5,6 +5,31 @@ defmodule Teamplace do
 
   alias Teamplace.Helpers
 
+  @doc """
+  Get Data, receives credentials type, resource (i.e. "reportes" || "facturaCompras"), action (i.e. "list")
+  and returns a response Map
+  """
+  @type credentials :: %{client_id: String.t(), client_secret: String.t()}
+  @spec get_data(credentials, String.t(), String.t(), Map.t()) :: Map.t()
+  def get_data(credentials, resource, action, params \\ nil) do
+    case HTTPoison.get!(
+           url_factory(credentials, resource, action, params),
+           [],
+           recv_timeout: :infinity
+         ) do
+      # status_code if invalid token
+      %HTTPoison.Response{status_code: 406, body: body} ->
+        new_token(credentials)
+        get_data(credentials, resource, action)
+
+      %HTTPoison.Response{body: ""} ->
+        []
+
+      %HTTPoison.Response{body: body} ->
+        Poison.decode!(body)
+    end
+  end
+
   def get_stream(credentials, resource, action, params \\ nil) do
     # Starts buffer
     buffer_pid = spawn(fn -> buffer() end)
@@ -23,6 +48,52 @@ defmodule Teamplace do
 
   def test() do
     get_stream(Mate.Accounts.get_user!(1).credentials, "reports", "saldosprov")
+  end
+
+  @doc """
+  Get's actual token is exists or generates new one
+
+  ## Examples
+
+    iex> Teamplace.get_token(%{client_id: System.get_env("TEAMPLACE_CLIENT_ID"), client_secret: System.get_env("TEAMPLACE_CLIENT_SECRET")})
+    ...> |> String.match?(~r/[\\d, \\w]{41}/)
+    true
+  """
+  def get_token(%{client_id: client_id} = credentials) do
+    Agent.get(:teamplace, & &1[client_id]) || new_token(credentials)
+  end
+
+  def post_data(credentials, resource, data) do
+    headers = [{"content-type", "application/json"}]
+    error = {:error, "Hubo un error"}
+
+    case HTTPoison.post(url_factory(credentials, resource), data, headers) do
+      %HTTPoison.Response{status_code: 406} ->
+        new_token(credentials)
+        post_data(credentials, resource, data)
+
+      {:ok, %HTTPoison.Response{status_code: 200}} ->
+        {:ok, "Registro Creado"}
+
+      {:ok, _} ->
+        error
+
+      {:error, _} ->
+        error
+    end
+  end
+
+  def url_factory(credentials, resource) do
+    Application.get_env(:teamplace, :api_base) <>
+      resource <>
+      "?ACCESS_TOKEN=" <> get_token(credentials)
+  end
+
+  def url_factory(credentials, resource, action, params \\ nil) do
+    Application.get_env(:teamplace, :api_base) <>
+      resource <>
+      "/" <>
+      action <> "?ACCESS_TOKEN=" <> get_token(credentials) <> Helpers.param_query_parser(params)
   end
 
   defp buffer({data, _status} = acc \\ {[], :stream}, remanent \\ "") do
@@ -45,7 +116,6 @@ defmodule Teamplace do
 
       %HTTPoison.AsyncEnd{} ->
         buffer({data, :end})
-
 
       {:deliver, request_pid} ->
         case acc do
@@ -94,78 +164,6 @@ defmodule Teamplace do
       fn _ -> nil end
     )
     |> Stream.map(&try_decode/1)
-  end
-
-  @doc """
-  Get Data, receives credentials type, resource (i.e. "reportes" || "facturaCompras"), action (i.e. "list")
-  and returns a response Map
-  """
-  @type credentials :: %{client_id: String.t(), client_secret: String.t()}
-  @spec get_data(credentials, String.t(), String.t(), Map.t()) :: Map.t()
-  def get_data(credentials, resource, action, params \\ nil) do
-    case HTTPoison.get!(
-           url_factory(credentials, resource, action, params),
-           [],
-           recv_timeout: :infinity
-         ) do
-      # status_code is invalid token
-      %HTTPoison.Response{status_code: 406, body: body} ->
-        new_token(credentials)
-        get_data(credentials, resource, action)
-
-      %HTTPoison.Response{body: ""} ->
-        []
-
-      %HTTPoison.Response{body: body} ->
-        Poison.decode!(body)
-    end
-  end
-
-  @doc """
-  Get's actual token is exists or generates new one
-
-  ## Examples
-
-    iex> Teamplace.get_token(%{client_id: System.get_env("TEAMPLACE_CLIENT_ID"), client_secret: System.get_env("TEAMPLACE_CLIENT_SECRET")})
-    ...> |> String.match?(~r/[\\d, \\w]{41}/)
-    true
-  """
-
-  def get_token(%{client_id: client_id} = credentials) do
-    Agent.get(:teamplace, & &1[client_id]) || new_token(credentials)
-  end
-
-  def post_data(credentials, resource, data) do
-    headers = [{"content-type", "application/json"}]
-    error = {:error, "Hubo un error"}
-
-    case HTTPoison.post(url_factory(credentials, resource), data, headers) do
-      %HTTPoison.Response{status_code: 406} ->
-        new_token(credentials)
-        post_data(credentials, resource, data)
-
-      {:ok, %HTTPoison.Response{status_code: 200}} ->
-        {:ok, "Registro Creado"}
-
-      {:ok, _} ->
-        error
-
-      {:error, _} ->
-        error
-    end
-  end
-
-  def url_factory(credentials, resource) do
-    Application.get_env(:teamplace, :api_base) <>
-      resource <>
-      "?ACCESS_TOKEN=" <> get_token(credentials)
-  end
-
-  def url_factory(credentials, resource, action, params \\ nil) do
-    Application.get_env(:teamplace, :api_base) <>
-      resource <>
-      "/" <>
-      action <> "?ACCESS_TOKEN=" <> get_token(credentials) <> Helpers.param_query_parser(params)
   end
 
   defp create_chunk_remanent([], partial_content, incomplete) do
@@ -228,14 +226,15 @@ defmodule Teamplace do
 
   defp new_token(credentials) do
     case auth_url(credentials)
-    |> HTTPoison.get!() do
+         |> HTTPoison.get!() do
       %HTTPoison.Response{status_code: 406} ->
         raise ArgumentError, message: "check client_id or client_secret"
+
       response ->
         response
         |> Map.get(:body)
         |> save_session(credentials)
-      end
+    end
   end
 
   defp auth_url(%{client_id: client_id, client_secret: client_secret}) do
