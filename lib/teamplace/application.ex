8fee417222d4a60cd09b2954ce285dbdac1803f8
Jose Esteves <jose@ceibo.co>
2019-06-21 11:15:14 -0300

    ensure envs are setted

--- lib/teamplace/application.ex
+++ lib/teamplace/application.ex
@@ -7,16 +7,26 @@ defmodule Teamplace.Application do
 
   def start(_type, _args) do
     # List all child processes to be supervised
+    ensure_required_envs() && IO.puts "Teampalce - All envs are correctly setted ✔"
+
     children = [
       # Starts a worker by calling: Teamplace.Worker.start_link(arg)
       # {Teamplace.Worker, arg},
-      {Teamplace.Worker, []},
-
+      {Teamplace.Worker, []}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Teamplace.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp ensure_required_envs do
+    credentials = Application.get_env(:teamplace, :credentials)
+    api_base = Application.get_env(:teamplace, :api_base)
+    credentials[:client_id] || raise "Missing TEAMPLACE_CLIENT_ID System Variable"
+    credentials[:client_secret] || raise "Missing TEAMPLACE_CLIENT_SECRET System Variable"
+    api_base || raise "Missing TEAMPLACE_API_BASE System Variable"
+    true
   end
 end
