b2c8d2d6f23f1b6bf515293833ba4eae6f2220b5
Jose Esteves <jose@ceibo.co>
2019-06-24 11:00:02 -0300

    beta

--- lib/teamplace/cobranza.ex
+++ lib/teamplace/cobranza.ex
@@ -30,8 +30,11 @@ defmodule Teamplace.Cobranza.Cotizacion do
 end
 
 # "AplicacionOrigen": "Tipo: String, Obligatorio: No, Descripción: Identificacion Externa de la transaccion origen a aplicar",
-
 defmodule Teamplace.Cobranza do
+  @moduledoc """
+    JSON example on doc/cobranza.json
+  """
+
   alias Teamplace.Cobranza
   alias Teamplace.Cobranza.{Banco, Otros, CtaCte, Cotizacion}
 
@@ -73,146 +76,3 @@ defmodule Teamplace.Cobranza do
     end)
   end
 end
-
-# {
-#   "IdentificacionExterna": "Tipo: String, Obligatorio: No, Descripción: Identificación externa",
-#   "EmpresaCodigo": "Tipo: String, Obligatorio: Si, Consulte la API /Empresa/list",
-#   "NumeroComprobante": "Tipo: String, Obligatorio: No, Descripción: Numero de documento",
-#   "Proveedor": "Tipo: String, Obligatorio: Si, Consulte la API /Proveedor/list",
-#       "TransaccionTipoCodigo": "Tipo: String, Obligatorio: Sí, Valor a completar: OPERTESORERIA ",
-#       "TransaccionSubtipoCodigo": "Tipo: String, Obligatorio: Sí, Consulte la API /TransaccionSubtipo/list",
-#       "Fecha": "Tipo: Date, Obligatorio: Si, Descripción: aaaa-mm-dd",
-#       "Nombre": "Tipo: String. Sólo para GET",
-#       "DiferenciaCambio": "Tipo: INT. Descripción: Coloque 0 para opción Asumir, 1 para No Asumir, 2 para opción Generar Documento, 3 para opción Segmentar",
-#       "UsaCotizacionOrigen": "Tipo: Boolean. Descripción: Coloque 0 para No, 1 para Sí",
-#       "Descripcion": "Tipo: String, Obligatorio: No",
-#       "CajaCodigo": "Tipo: String, Obligatorio: No. Consulte la API /Caja/list",
-#       "CobradorCodigo": "Tipo: String, Consulte la API /Persona/listAll (No obligatoria)",
-#       "Banco": [
-#         {
-#           "OperacionBancariaCodigo": "Tipo: String, Obligatorio: Sí, Consulte la API /OperacionBancaria/list",
-#           "CuentaCodigo": "Tipo: String, Obligatorio: Sí, Consulte la API /Cuenta/list",
-#           "DebeHaber": "Tipo: INT. Descripción: Coloque 1 para Debe o -1 para Haber",
-#           "ImporteMonTransaccion": "Tipo: Decimal. Obligatorio: Sí",
-#           "MonedaCodigo": "Tipo: String, Obligatorio: Sí, Consulte la API /Moneda/list",
-#           "Descripcion": "Tipo: String, Obligatorio: No",
-#           "DocumentoFisicoID": "Tipo: int, Obligatorio: No",
-#           "NumeroDocumentoFisico": null,
-#           "FechaDocumentoFisico": "Tipo: Date, Obligatorio: No, Descripción: aaaa-mm-dd",
-#           "FechaVencimientoDocumentoFisico": "Tipo: Date, Obligatorio: No, Descripción: aaaa-mm-dd",
-#           "BancoCodigo": "Tipo: String, Obligatorio: No, Consulte la API /Banco/list",
-#           "DimensionDistribucion": [
-#             {
-#               "dimensionCodigo": "(Tipo) String, (Obligatorio) Si, si lo requiere la transacción. Consulte la API /Dimension/list",
-#               "distribucionCodigo": "(Tipo) String, (Obligatorio) No, Pasar el codigo de la dimensión distribucion en caso de querer pasar una distribución por default. No sería necesario pasar distribucionItems",
-#               "distribucionItems": [
-#                 {
-#                   "codigo": "(Tipo) String, (Obligatorio) No, Codigo de registro de la dimension. Ej.: Código de un registro perteneciente a la dimensión Centro de Costo.",
-#                   "porcentaje": "(Tipo) Numero, (Obligatorio) No, Porcentaje de distribución"
-#                 }
-#               ]
-#             }
-#           ]
-#         }
-#       ],
-#       "Efectivo": [
-#         {
-#           "CuentaCodigo": "Tipo: String, Obligatorio: Sí, Consulte la API /Cuenta/list",
-#           "DebeHaber": "Tipo: INT. Descripción: Coloque 1 para Debe o -1 para Haber",
-#           "ImporteMonTransaccion": "Tipo: Decimal. Obligatorio: Sí",
-#           "MonedaCodigo": "Tipo: String, Obligatorio: Sí, Consulte la API /Moneda/list",
-#           "Descripcion": "Tipo: String, Obligatorio: No",
-#           "DimensionDistribucion": [
-#             {
-#               "dimensionCodigo": "(Tipo) String, (Obligatorio) Si, si lo requiere la transacción. Consulte la API /Dimension/list",
-#               "distribucionCodigo": "(Tipo) String, (Obligatorio) No, Pasar el codigo de la dimensión distribucion en caso de querer pasar una distribución por default. No sería necesario pasar distribucionItems",
-#               "distribucionItems": [
-#                 {
-#                   "codigo": "(Tipo) String, (Obligatorio) No, Codigo de registro de la dimension. Ej.: Código de un registro perteneciente a la dimensión Centro de Costo.",
-#                   "porcentaje": "(Tipo) Numero, (Obligatorio) No, Porcentaje de distribución"
-#                 }
-#               ]
-#             }
-#           ]
-#         }
-#       ],
-#       "Tarjeta": [
-#         {
-#           "OperacionBancariaCodigo": "Tipo: String, Obligatorio: Sí, Consulte la API /OperacionBancaria/list",
-#           "CuentaCodigo": "Tipo: String, Obligatorio: Sí, Consulte la API /Cuenta/list",
-#           "DebeHaber": "Tipo: INT. Descripción: Coloque 1 para Debe o -1 para Haber",
-#           "ImporteMonTransaccion": "Tipo: Decimal. Obligatorio: Sí",
-#           "MonedaCodigo": "Tipo: String, Obligatorio: Sí, Consulte la API /Moneda/list",
-#           "Descripcion": "Tipo: String, Obligatorio: No",
-#           "NumeroDocumentoFisico": null,
-#           "DocumentoTitular": "Tipo: String, Obligatorio: No",
-#           "FechaDocumento": "Tipo: Date, Obligatorio: No, Descripción: aaaa-mm-dd",
-#           "FechaVencimiento": "Tipo: Date, Obligatorio: No, Descripción: aaaa-mm-dd",
-#           "NumeroAutorizacionTarjeta": "Tipo: String, Obligatorio: No",
-#           "NumeroTarjeta": "Tipo: String, Obligatorio: No",
-#           "NumeroLote": "Tipo: String, Obligatorio: No",
-#           "Titular": "Tipo: String, Obligatorio: No",
-#           "BancoCodigo": "Tipo: String, Obligatorio: No, Consulte la API /Banco/list"
-#         }
-#       ],
-#       "Otros": [
-#         {
-#           "CuentaCodigo": "Tipo: String, Obligatorio: Sí, Consulte la API /Cuenta/list",
-#           "DebeHaber": "Tipo: INT. Descripción: Coloque 1 para Debe o -1 para Haber",
-#           "ImporteMonTransaccion": "Tipo: Decimal. Obligatorio: Sí.",
-#           "MonedaCodigo": "Tipo: String, Obligatorio: Sí, Consulte la API /Moneda/list",
-#           "Descripcion": "Tipo: String, Obligatorio: No",
-#           "DimensionDistribucion": [
-#             {
-#               "dimensionCodigo": "(Tipo) String, (Obligatorio) Si, si lo requiere la transacción. Consulte la API /Dimension/list",
-#               "distribucionCodigo": "(Tipo) String, (Obligatorio) No, Pasar el codigo de la dimensión distribucion en caso de querer pasar una distribución por default. No sería necesario pasar distribucionItems",
-#               "distribucionItems": [
-#                 {
-#                   "codigo": "(Tipo) String, (Obligatorio) No, Codigo de registro de la dimension. Ej.: Código de un registro perteneciente a la dimensión Centro de Costo.",
-#                   "porcentaje": "(Tipo) Numero, (Obligatorio) No, Porcentaje de distribución"
-#                 }
-#               ]
-#             }
-#           ]
-#         }
-#       ],
-#       "Retenciones": [
-#         {
-#           "RetencionCodigo": "Tipo: String, Obligatorio: No, Consulte la API /Retencion/list",
-#           "Importe": "Tipo: Decimal. Obligatorio: Sí",
-#           "ISAR": "Tipo: Decimal. Obligatorio: No",
-#           "ISARAcumulado": "Tipo: Decimal. Obligatorio: No",
-#           "Fecha": "Tipo: Date, Obligatorio: Si, Descripción: aaaa-mm-dd",
-#           "NumeroRetencion": "Tipo: String, Obligatorio: Si"
-#         }
-#       ],
-#       "Cotizaciones": [
-#         {
-#           "MonedaCodigo": "Tipo: String, Obligatorio: No, Consulte la API /Moneda/list",
-#           "Cotizacion": "Tipo: Decimal. Obligatorio: No"
-#         }
-#       ],
-#       "CtaCte": [
-#         {
-#           "CuentaCodigo": "Tipo: String, Obligatorio: Sí, Consulte la API /Cuenta/list",
-#           "DebeHaber": "Tipo: INT. Descripción: Coloque 1 para Debe o -1 para Haber",
-#           "ImporteMonTransaccion": "Tipo: Decimal. Obligatorio: Sí",
-#           "ImporteMonPrincipal": "Tipo: Decimal. Obligatorio: Sí",
-#           "MonedaCodigo": "Tipo: String, Obligatorio: Sí, Consulte la API /Moneda/list",
-#           "Descripcion": "Tipo: String, Obligatorio: No",
-#           "AplicacionOrigen": "Tipo: String, Obligatorio: No, Descripción: Identificacion Externa de la transaccion origen a aplicar",
-#           "DimensionDistribucion": [
-#             {
-#               "dimensionCodigo": "(Tipo) String, (Obligatorio) Si, si lo requiere la transacción. Consulte la API /Dimension/list",
-#               "distribucionCodigo": "(Tipo) String, (Obligatorio) No, Pasar el codigo de la dimensión distribucion en caso de querer pasar una distribución por default. No sería necesario pasar distribucionItems",
-#               "distribucionItems": [
-#                 {
-#                   "codigo": "(Tipo) String, (Obligatorio) No, Codigo de registro de la dimension. Ej.: Código de un registro perteneciente a la dimensión Centro de Costo.",
-#                   "porcentaje": "(Tipo) Numero, (Obligatorio) No, Porcentaje de distribución"
-#                 }
-#               ]
-#             }
-#           ]
-#         }
-#       ]
-#     }
